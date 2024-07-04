deec058bba08b157817694c091c397c402f9d198
RosenlindDylan <35314066+RosenlindDylan@users.noreply.github.com>
2024-07-03 21:29:58 -0600

    precision fix

--- main.cpp
+++ main.cpp
@@ -2,32 +2,30 @@
 #include <vector>
 #include <random>
 #include <cmath>
+#include <iomanip>
 #include <pthread.h>
 
 struct ThreadData {
-    int* result;
+    long double* result;
     long double num_samples;
 };
 
 void* calculate_orthant(void *thread_args) {
-
     ThreadData* data = (ThreadData*)thread_args;
-    int* result = data->result;
+    long double* result = data->result;
     long double num_samples = data->num_samples;
 
-    int inside;
-    // threadsafe random number generator
+    long double inside = 0;
     std::random_device rd;
     std::mt19937 generator(rd());
-    std::uniform_real_distribution<long double> distribution(0, 1); // [0, 1) which is a small issue
+    std::uniform_real_distribution<long double> distribution(0.0L, 1.0L);
 
-    for (int i = 0; i < num_samples; i++) {
+    for (long double i = 0; i < num_samples; i++) {
         long double x_comp = distribution(generator);
         long double y_comp = distribution(generator);
         
-        // take norm of vector
         long double norm = sqrt(x_comp * x_comp + y_comp * y_comp);
-        if (norm <= 1) {
+        if (norm <= 1.0L) {
             inside++;
         }
     }
@@ -37,38 +35,31 @@ void* calculate_orthant(void *thread_args) {
 }
 
 int main() {
-    
-    // assign amount of threads and samples, create vectors for points and threads
     int num_threads = 4;
-    long double num_samples = 10000000.;
-    std::vector<int> flummoxes(num_threads);
+    long double num_samples = 10000000.0L;
+    std::vector<long double> flummoxes(num_threads);
     pthread_t threads[num_threads];
     std::vector<ThreadData> thread_data(num_threads);
 
-    int total_inside = 0;
+    long double total_inside = 0.0L;
 
-    // Create threads and pass indices of vectors by reference
     for (int i = 0; i < num_threads; ++i) {
         thread_data[i].result = &flummoxes[i];
         thread_data[i].num_samples = num_samples;
         pthread_create(&threads[i], NULL, calculate_orthant, (void*)&thread_data[i]);
     }
 
-    // join threads
     for (int j = 0; j < num_threads; j++) {
         pthread_join(threads[j], NULL); 
     }
 
-
-    // calculate end result
     for (int k = 0; k < num_threads; k++) {
         total_inside += flummoxes[k];
     }
-    
-    long double pi = num_threads * (static_cast<long double>(total_inside) / (num_samples * num_threads));
 
-    std::cout << "After " << num_samples << " iterations on each of " << num_threads << " threads, the value of pi = " << pi << std::endl;
+    long double pi = 4.0L * total_inside / (num_samples * num_threads);
 
+    std::cout << "After " << num_samples << " iterations on each of " << num_threads << " threads, the value of pi = " << std::setprecision(20) << pi << std::endl;
 
     return 0;
 }
